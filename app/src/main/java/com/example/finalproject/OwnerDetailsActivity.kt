e8e6a7d2692f92bda0738284c772a58b81da5553
sazzad369 <mdsazzad0369@gmail.com>
2024-10-06 13:36:11 +0600

    update

---
+++ app/src/main/java/com/example/finalproject/OwnerDetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class OwnerDetailsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setSupportActionBar(R.layout.activity_owner_details)
+
+    }
+}
